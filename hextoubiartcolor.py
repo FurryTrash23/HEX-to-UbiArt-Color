9b26353f2a31160b5c5acc9fa496f0b3678ed8f0
WorteJD <89742649+WorteJD@users.noreply.github.com>
2021-09-26 19:51:35 +0200

    Update hextoubiartcolor.py

--- hextoubiartcolor.py
+++ hextoubiartcolor.py
@@ -1,3 +1,12 @@
 print('HEX to UAC (UbiArt color) convert\nby Worte\n')
-rgb = tuple(int(input('Enter hex: ').lstrip('#')[i:i+2], 16) for i in (0, 2, 4))
-print([].append(rgb[0] / 255).append(rgb[1] / 255).append(rgb[2] / 255).append(input('Enter transparency (0-100): ') * 2.55 / 255))
+hex = input('Enter hex: ').lstrip('#')
+transparency = input('Enter transparency (0-100): ')
+rgb = tuple(int(hex[i:i+2], 16) for i in (0, 2, 4))
+output = []
+output.append(rgb[0] / 255)
+output.append(rgb[1] / 255)
+output.append(rgb[2] / 255)
+output.append(float(transparency) * 2.55 / 255)
+print('If some of the values are something like "0.99999999999999999" you can round it if you want to.')
+uac = output
+print(uac)
