da68afb780169c2d1ccca9ae42c7c40a8c8e8071
WorteJD <89742649+WorteJD@users.noreply.github.com>
2021-09-26 20:34:11 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ hex = input('Enter hex: ').lstrip('#')
 transparency = input('Enter transparency (0-100): ')
 rgb = tuple(int(hex[i:i+2], 16) for i in (0, 2, 4))
 output = []
-output.append(round(float(transparency) / 100, 6))
-output.append(round(rgb[0] / 255, 6))
-output.append(round(rgb[0] / 255, 6))
-output.append(round(rgb[0] / 255, 6))
+output.append(str(float(transparency) / 100)[:8])
+output.append(str(rgb[0] / 255)[:8])
+output.append(str(rgb[0] / 255)[:8])
+output.append(str(rgb[0] / 255)[:8])
 uac = output
 print(uac)
