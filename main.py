c1bb5e48561f2aa45f67d222c9370492ae073bf3
FurryTrash23 <100256794+FurryTrash23@users.noreply.github.com>
2024-04-08 12:04:50 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 print('HEX to UAC (UbiArt color) convert\nby Worte\n')
-hex = input('Enter hex: ').lstrip('#')
+
+# Taking input for HEX and transparency
+hex_value = input('Enter hex: ').lstrip('#')
 transparency = input('Enter transparency (0-100): ')
-rgb = tuple(int(hex[i:i+2], 16) for i in (0, 2, 4))
-output = []
-output.append(str(float(transparency) / 100)[:8])
-output.append(str(rgb[0] / 255)[:8])
-output.append(str(rgb[1] / 255)[:8])
-output.append(str(rgb[2] / 255)[:8])
-uac = str(output).replace("'", "")
+
+# Converting HEX to RGB
+rgb = tuple(int(hex_value[i:i+2], 16) for i in (0, 2, 4))
+
+# Calculating UAC values
+uac_transparency = str(float(transparency) / 100)[:8]
+uac_red = str(rgb[0] / 255)[:8]
+uac_green = str(rgb[1] / 255)[:8]
+uac_blue = str(rgb[2] / 255)[:8]
+
+# Forming the UAC string
+uac = f'{uac_transparency},{uac_red},{uac_green},{uac_blue}'
+
+# Printing UAC
 print(uac)
+
+# Writing UAC to a text file
+with open('ubiartcolor.txt', 'w') as file:
+    file.write(uac)
